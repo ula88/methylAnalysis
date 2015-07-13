e4eda45af1bd49f3c8fc240b6b372b27c4f241c9
globalna_ula <ulanowakowska88@wp.pl>
2015-07-13 13:45:55 +0200

    add runtime

--- methylAnalysis/src/main/java/methyl/Controller.java
+++ methylAnalysis/src/main/java/methyl/Controller.java
@@ -1,10 +1,27 @@
-
 package methyl;
+
+import java.io.IOException;
 
 /**
  *
  * @author Ula
  */
 public class Controller {
-    
+
+    String samples = "test1.CpG.txt,test2.CpG.txt,control1.CpG.txt,control2.CpG.txt";
+    String treatment = "1,1,0,0";
+    String mydiffFile = "proba1_diff.txt";
+    String myDMRfile = "proba1_dmr.txt";
+
+    public void runRscript() throws IOException, InterruptedException {
+        String pathToScript = "/media/eternus/NGS_data/DMR/dmrWithParam.R";
+        Process child = Runtime.getRuntime().exec("Rscript " + pathToScript + " "
+                + samples + " " + treatment + " " + mydiffFile + " " + myDMRfile);
+        child.waitFor();
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Controller con = new Controller();
+        con.runRscript();
+    }
 }
